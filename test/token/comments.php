19e2bacb7971b7500b235ac6b08a4abdb1fa9b4b
Ioan CHIRIAC <i.chiriac@yahoo.com>
2017-01-16 21:01:31 +0100

    try comments

---
+++ test/token/comments.php
@@ -0,0 +1,11 @@
+<?php
+if /* ignore */ (/* */ true) /* ignore */ {
+# inner statement
+} /* ignore */ else /* ignore */
+  if (true /* ignore */) /* ignore */ {
+  } /* ignore */ elseif /* ignore */ (/* ignore */ false /* ignore */ /* ignore */) /* ignore */ /* ignore */ {
+  } /* ignore */ else /* ignore */ {
+  }
+if (false) /* ignore */ : /* ignore */ ?>
+hello world <?php
+/* ignore */ endif /* ignore */;/* ignore */
