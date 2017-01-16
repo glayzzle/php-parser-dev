2da922810e15e4a077883ff4635edc150db88f23
Ioan CHIRIAC <i.chiriac@yahoo.com>
2017-01-16 21:19:50 +0100

    improve tests on comments - https://github.com/glayzzle/php-parser/issues/56

--- test/token/comments.php
+++ test/token/comments.php
@@ -1,11 +1,71 @@
 <?php
-if /* ignore */ (/* */ true) /* ignore */ {
-# inner statement
-} /* ignore */ else /* ignore */
-  if (true /* ignore */) /* ignore */ {
-  } /* ignore */ elseif /* ignore */ (/* ignore */ false /* ignore */ /* ignore */) /* ignore */ /* ignore */ {
-  } /* ignore */ else /* ignore */ {
+namespace  /*?*/ a  /*?*/ \  /*?*/ b  /*?*/ {
+
+  if /* ignore */ (/* */ true) /* ignore */ {
+  # inner statement
+  } /* ignore */ else /* ignore */
+    if (true /* ignore */) /* ignore */ {
+    } /* ignore */ elseif /* ignore */ (/* ignore */ false /* ignore */ /* ignore */) /* ignore */ /* ignore */ {
+    } /* ignore */ else /* ignore */ {
+    }
+  if (false) /* ignore */ : /* ignore */ ?>
+  hello world <?php
+  /* ignore */ endif /* ignore */;/* ignore */
+
+  for /*?*/ ( /*?*/ $i /*?*/ = /*?*/ 0 /*?*/; /*?*/ $i /*?*/ < /*?*/ 10 /*?*/; /*?*/ $i++ /*?*/ ) /*?*/ {
+    /*?*/
   }
-if (false) /* ignore */ : /* ignore */ ?>
-hello world <?php
-/* ignore */ endif /* ignore */;/* ignore */
+
+  do /*?*/ {
+    /*?*/
+  } /*?*/ while /*?*/ (/*?*/ false /*?*/) /*?*/;
+
+  while /*?*/ (/*?*/ false /*?*/) /*?*/ {
+    /*?*/
+  }
+
+  foreach /*?*/ ($a /*?*/ as /*?*/ $i /*?*/ => /*?*/ $j /*?*/ ) /*?*/ {
+    /*?*/
+  }
+
+  switch /*?*/ (/*?*/ $a /*?*/) /*?*/ {
+    /*?*/ case /*?*/ 1 /*?*/ :
+    /*?*/ break /*?*/ ;
+    /*?*/ default /*?*/;
+  }
+
+  function /*?*/ &foo /*?*/ (/*?*/ array /*?*/ &$a /*?*/ = /*?*/ null /*?*/ ) /*?*/ : array /*?*/ {
+    return /*?*/ null /*?*/ ; /*?*/
+  } /*?*/
+
+  foo /*?*/ ( /*?*/ true /*?*/ , /*?*/ false /*?*/ )/*?*/;
+
+  $a /*?*/ = /*?*/ [ /*?*/ true /*?*/, /*?*/ "aa" /*?*/ => /*?*/ false /*?*/ ];
+
+  list /*?*/ ( /*?*/ $a /*?*/,  /*?*/ ,  /*?*/ list  /*?*/ ( /*?*/ $b /*?*/ )
+   /*?*/ )  /*?*/ = /*?*/ [ /*?*/ true /*?*/,
+  /*?*/ "aa" /*?*/ => /*?*/ false /*?*/ ];
+
+  for /*?*/ ( /*?*/ $i /*?*/ = /*?*/ 0 /*?*/;
+  /*?*/ $i /*?*/ < /*?*/ 10 /*?*/; /*?*/ $i++ /*?*/ ) /*?*/ :
+    /*?*/
+  endfor /*?*/ ;
+
+  do /*?*/ {
+    /*?*/
+  } /*?*/ while /*?*/ (/*?*/ false /*?*/) /*?*/;
+
+  while /*?*/ (/*?*/ false /*?*/) /*?*/  :
+    /*?*/
+  endwhile /*?*/;
+
+  foreach /*?*/ ($a /*?*/ as /*?*/ $i /*?*/ => /*?*/ $j /*?*/ ) /*?*/ :
+    /*?*/
+  endforeach /*?*/ ;
+
+  switch /*?*/ (/*?*/ $a /*?*/) /*?*/ :
+    /*?*/ case /*?*/ 1 /*?*/ :
+    /*?*/ break /*?*/ ;
+    /*?*/ default /*?*/;
+  endswitch /*?*/ ; 
+}
